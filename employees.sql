af26c341345bdc8d3b10ae07ab2dd86034c5fe15
Gayan Kaveenda <44737130+gayankaveenda@users.noreply.github.com>
2025-04-30 22:59:17 +1000

    Update employees.sql
    fixed bug

--- employees.sql
+++ employees.sql
@@ -110,18 +110,18 @@ CREATE OR REPLACE VIEW current_dept_emp AS
 flush /*!50503 binary */ logs;
 
 SELECT 'LOADING departments' as 'INFO';
-source load_departments.dump ;
+source C:/workspace/mysql/load_departments.dump ;
 SELECT 'LOADING employees' as 'INFO';
-source load_employees.dump ;
+source C:/workspace/mysql/load_employees.dump ;
 SELECT 'LOADING dept_emp' as 'INFO';
-source load_dept_emp.dump ;
+source C:/workspace/mysql/load_dept_emp.dump ;
 SELECT 'LOADING dept_manager' as 'INFO';
-source load_dept_manager.dump ;
+source C:/workspace/mysql/load_dept_manager.dump ;
 SELECT 'LOADING titles' as 'INFO';
-source load_titles.dump ;
+source C:/workspace/mysql/load_titles.dump ;
 SELECT 'LOADING salaries' as 'INFO';
-source load_salaries1.dump ;
-source load_salaries2.dump ;
-source load_salaries3.dump ;
+source C:/workspace/mysql/load_salaries1.dump ;
+source C:/workspace/mysql/load_salaries2.dump ;
+source C:/workspace/mysql/load_salaries3.dump ;
 
-source show_elapsed.sql ;
+source C:/workspace/mysql/show_elapsed.sql ;
